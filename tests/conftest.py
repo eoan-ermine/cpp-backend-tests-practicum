981a26791efeb1c39755b7fd0c3043fb278bfad7
Nickolay Kolesnikov <nick206265@gmail.com>
2022-06-28 17:36:22 +0300

    tests

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import os
+import pytest
+import requests
+
+from xprocess import ProcessStarter
+from dataclasses import dataclass
+from urllib.parse import urljoin
+from pathlib import Path
+
+
+@dataclass
+class Server:
+    url: str
+
+    def get(self, endpoint):
+        return requests.get(urljoin(self.url, endpoint))
+
+    def post(self, endpoint, data):
+        return requests.post(urljoin(self.url, endpoint), data)
+
+
+@pytest.fixture(scope='module')
+def myserver(xprocess):
+    path = os.environ['DELIVERY_APP']
+    if not Path(path).exists():
+        raise Exception(f"no such file {os.environ['DELIVERY_APP']}")
+
+    class Starter(ProcessStarter):
+        pattern = 'Running on'
+        args = ['python3', path]
+
+    xprocess.ensure("myserver", Starter)
+
+    yield Server('http://localhost:8000/')
+
+    xprocess.getinfo("myserver").terminate()
