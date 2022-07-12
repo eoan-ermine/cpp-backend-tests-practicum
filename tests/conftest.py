8c2ea852fa32001498bcd2ffe3055ebf828b1817
Nickolay Kolesnikov <nick206265@gmail.com>
2022-07-12 18:36:46 +0300

    test empty pattern

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,7 +26,7 @@ def myserver(xprocess):
         raise Exception(f"no such file {os.environ['DELIVERY_APP']}")
 
     class Starter(ProcessStarter):
-        pattern = 'Running on'
+        pattern = ''
         args = [path]
 
     xprocess.ensure("myserver", Starter)
