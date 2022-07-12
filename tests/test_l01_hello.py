bd77edc8e88dd2c95c861ab4d2ea3eb1800d9174
Nickolay Kolesnikov <nick206265@gmail.com>
2022-07-12 17:55:38 +0300

    another fix

--- tests/test_l01_hello.py
+++ tests/test_l01_hello.py
@@ -2,4 +2,4 @@ def test_hello(myserver):
     name = 'Baron'
     res = myserver.get(f'/{name}')
     assert res.status_code == 200
-    assert res.text == f'Hello, {name}!'
+    assert res.text == f'Hello, {name}'
