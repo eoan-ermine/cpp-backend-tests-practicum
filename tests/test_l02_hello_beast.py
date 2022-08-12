2b05023aa29cd1889396ef7e4d42a7d7afb043b0
Nickolay Kolesnikov <nick206265@gmail.com>
2022-08-12 18:33:43 +0300

    fix

--- tests/test_l02_hello_beast.py
+++ tests/test_l02_hello_beast.py
@@ -4,7 +4,7 @@ def test_hello(myserver):
     assert res.status_code == 200
     assert res.text == f'Hello, {name}'
     assert res.headers['content-type'] == 'text/html'
-    assert res.headers['content-length'] == 12
+    assert res.headers['content-length'] == '12'
 
 def test_hello_error(myserver):
     name = 'Baron'
@@ -12,4 +12,4 @@ def test_hello_error(myserver):
     assert res.status_code == 405
     assert res.text == f'Invalid method'
     assert res.headers['content-type'] == 'text/html'
-    assert res.headers['content-length'] == 14
+    assert res.headers['content-length'] == '14'
