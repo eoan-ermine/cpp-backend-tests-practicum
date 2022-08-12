d5dffbe666009f04da72b65e7db05c9a538e63a2
Nickolay Kolesnikov <nick206265@gmail.com>
2022-08-12 18:25:16 +0300

    fix status code

--- tests/test_l02_hello_beast.py
+++ tests/test_l02_hello_beast.py
@@ -9,7 +9,7 @@ def test_hello(myserver):
 def test_hello_error(myserver):
     name = 'Baron'
     res = myserver.post(f'/{name}', f'some')
-    assert res.status_code == 400
+    assert res.status_code == 405
     assert res.text == f'Invalid method'
     assert res.headers['content-type'] == 'text/html'
     assert res.headers['content-length'] == 14
