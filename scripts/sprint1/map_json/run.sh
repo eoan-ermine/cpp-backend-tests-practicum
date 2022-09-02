94c302e7a7b2223a340e791ad4ce735ba3733cf5
Oleg Skinderev <>
2022-09-02 12:02:44 +0300

    replace python3.9 to python3

--- scripts/sprint1/map_json/run.sh
+++ scripts/sprint1/map_json/run.sh
@@ -6,4 +6,4 @@ export DELIVERY_APP=${REPO}/sprint1/problems/map_json/solution/build/bin/game_se
 
 export CONFIG_PATH=${REPO}/sprint1/problems/map_json/solution/data/config.json
 
-python3.9 -m pytest --rootdir=${REPO} --verbose --junitxml=results.xml cpp-backend-tests-practicum/tests/test_l04_map_json.py
+python3 -m pytest --rootdir=${REPO} --verbose --junitxml=results.xml cpp-backend-tests-practicum/tests/test_l04_map_json.py
