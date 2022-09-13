f4c9135e544fade47e2dbf4e2a304764d2c17120
Oleg Skinderev <>
2022-09-13 21:06:24 +0300

    fix envs

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from xprocess import ProcessStarter
 from dataclasses import dataclass
 from urllib.parse import urljoin
 from pathlib import Path
+from typing import Optional
 
 
 @dataclass
@@ -19,17 +20,30 @@ class Server:
         return requests.post(urljoin(self.url, endpoint), data)
 
 
+def is_env_path(env_name: str) -> Optional[str]:
+    path = os.environ.get(env_name)
+    if path and not Path(path).exists():
+        raise FileNotFoundError(f"no such file {path}")
+    return path
+
+
 @pytest.fixture(scope='module')
 def myserver(xprocess):
-    path = os.environ['DELIVERY_APP']
-    if not Path(path).exists():
-        raise Exception(f"no such file {os.environ['DELIVERY_APP']}")
-    config_path = os.environ.get('CONFIG_PATH')
-    data_path = os.environ.get('DATA_PATH')
+    path = is_env_path('DELIVERY_APP')
+    config_path = is_env_path('CONFIG_PATH')
+    data_path = is_env_path('DATA_PATH')
+
+    args_ = list()
+    if path:
+        args_.append(path)
+    if config_path:
+        args_.append(config_path)
+    if data_path:
+        args_.append(data_path)
 
     class Starter(ProcessStarter):
         pattern = 'Server has started...'
-        args = [path, config_path, data_path]
+        args = args_
 
     xprocess.ensure("myserver", Starter)
     yield Server('http://127.0.0.1:8080/')
