f32f29b95f5b8cc86e61238671539af3ebf0918f
Oleg Skinderev <>
2022-10-07 17:05:21 +0300

    add test for join_game

--- tests/test_s02_join_game.py
+++ tests/test_s02_join_game.py
@@ -17,12 +17,17 @@ def get_maps(myserver_in_docker):
     return res.json()
 
 
-def test_join_success(myserver_in_docker):
+def join_to_map(myserver_in_docker, user_name, map_id):
     request = 'api/v1/game/join'
     header = {'content-type': 'application/json'}
     maps = get_maps(myserver_in_docker)
-    data = {"userName": "Harry Potter", "mapId": maps[0]['id']}
-    res = myserver_in_docker.request('POST', header, request, json=data)
+    data = {"userName": user_name, "mapId": map_id}
+    return myserver_in_docker.request('POST', header, request, json=data)
+
+
+def test_join_success(myserver_in_docker):
+    maps = get_maps(myserver_in_docker)
+    res = join_to_map(myserver_in_docker, 'Harry Potter', maps[0]['id'])
     assert res.status_code == 200
     assert res.headers['content-type'] == 'application/json'
     assert res.headers['cache-control'] == 'no-cache'
@@ -110,7 +115,7 @@ def test_join_invalid_data(myserver_in_docker):
     res_json = res.json()
 
 
-def test_join_invalid_data(myserver_in_docker):
+def test_join_invalid_verb(myserver_in_docker):
     request = 'api/v1/game/join'
     header = {'content-type': 'application/json'}
     data = {"userName": "", "mapId": '   invalid   '}
@@ -119,3 +124,66 @@ def test_join_invalid_data(myserver_in_docker):
     assert res.headers['content-type'] == 'application/json'
     assert res.headers['cache-control'] == 'no-cache'
     res_json = res.json()
+    print(res_json)
+    assert res_json['code'] == 'invalidMethod'
+
+
+def test_players_success(myserver_in_docker):
+    maps = get_maps(myserver_in_docker)
+    res = join_to_map(myserver_in_docker, 'User1', maps[0]['id'])
+    token1 = res.json()['authToken']
+    res = join_to_map(myserver_in_docker, 'User2', maps[0]['id'])
+    token2 = res.json()['authToken']
+
+    request = 'api/v1/game/players'
+    header = {'content-type': 'application/json', 'authorization': f'Bearer {token1}'}
+    res = myserver_in_docker.request('GET', header, request)
+    assert res.status_code == 200
+    assert res.headers['content-type'] == 'application/json'
+    assert res.headers['cache-control'] == 'no-cache'
+    res_json1 = res.json()
+
+    header = {'content-type': 'application/json', 'authorization': f'Bearer {token2}'}
+    res = myserver_in_docker.request('GET', header, request)
+    assert res.status_code == 200
+    assert res.headers['content-type'] == 'application/json'
+    assert res.headers['cache-control'] == 'no-cache'
+    res_json2 = res.json()
+
+    assert res_json1 == res_json2
+
+
+def test_players_miss_token(myserver_in_docker):
+    request = 'api/v1/game/players'
+    header = {'content-type': 'application/json'}
+    res = myserver_in_docker.request('GET', header, request)
+    assert res.status_code == 401
+    assert res.headers['content-type'] == 'application/json'
+    assert res.headers['cache-control'] == 'no-cache'
+    res_json = res.json()
+    print(res_json)
+    assert res_json['code'] == 'invalidToken'
+
+
+def test_players_invalid_token(myserver_in_docker):
+    request = 'api/v1/game/players'
+    header = {'content-type': 'application/json', 'authorization': 'Bearer'}
+    res = myserver_in_docker.request('GET', header, request)
+    assert res.status_code == 401
+    assert res.headers['content-type'] == 'application/json'
+    assert res.headers['cache-control'] == 'no-cache'
+    res_json = res.json()
+    print(res_json)
+    assert res_json['code'] == 'invalidToken'
+
+
+def test_players_unknown_token(myserver_in_docker):
+    request = 'api/v1/game/players'
+    header = {'content-type': 'application/json', 'authorization': 'Bearer 6516861d89ebfff147bf2eb2b5153ae1'}
+    res = myserver_in_docker.request('GET', header, request)
+    assert res.status_code == 401
+    assert res.headers['content-type'] == 'application/json'
+    assert res.headers['cache-control'] == 'no-cache'
+    res_json = res.json()
+    print(res_json)
+    assert res_json['code'] == 'unknownToken'
