d8ff26d56a90d965393efe2e9c00d10b623658fa
Oleg Skinderev <>
2022-10-21 19:40:56 +0300

    add tests

---
+++ tests/test_s03_instrumentation.py
@@ -0,0 +1,24 @@
+import os
+import json
+import time
+import subprocess
+
+from pathlib import Path
+
+
+def test_report():
+    report_path = Path(os.environ['REPORT_PATH'])
+    report = json.loads(report_path.read_text())
+    assert report['slowest_func_v0'] == 'addAnnotatedEdge'
+    assert report['slowest_func_v1'] == 'getNode'
+    assert report['slowest_func_v2']
+
+
+def test_time():
+    binary_path = os.environ['BINARY_PATH']
+    proc = subprocess.Popen(binary_path, shell=True)
+    start = time.time()
+    proc.wait()
+    delta = time.time() - start
+    print(delta)
+    assert False
