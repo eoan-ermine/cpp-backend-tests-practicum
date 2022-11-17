51cf49f310be7471f1f030ac7bc4a57752df78ef
Nickolay Kolesnikov <nick206265@gmail.com>
2022-11-17 15:58:34 +0300

    load

---
+++ tests/test_s03_load.py
@@ -0,0 +1,45 @@
+import os, glob
+
+from pathlib import Path
+
+import pytest
+
+import numpy as np
+
+
+@pytest.fixture
+def directory():
+    return Path(os.environ['DIRECTORY'])
+
+
+def test_only_200(directory):
+    logdirname = max(glob.glob(os.path.join(os.environ['DIRECTORY'], '*/')), key=os.path.getctime)
+    filename = ''
+    for file_name in os.listdir(logdirname):
+        name, end = os.path.splitext(file_name)
+        if name.startswith('phout_') and end == '.log':
+            filename = file_name
+
+    with open(os.path.join(logdirname, filename)) as phout:
+        lines = phout.readlines()
+        for line in lines:
+            assert line.split()[-1] == '200'
+
+def test_percentiles(directory):
+    logdirname = max(glob.glob(os.path.join(os.environ['DIRECTORY'], '*/')), key=os.path.getctime)
+    filename = ''
+    for file_name in os.listdir(logdirname):
+        name, end = os.path.splitext(file_name)
+        if name.startswith('phout_') and end == '.log':
+            filename = file_name
+
+    with open(os.path.join(logdirname, filename)) as phout:
+        lines = phout.readlines()
+        timings = []
+        for line in lines:
+            timings.append(line.split()[-10])
+        arr = np.array(timings)
+        p50 = np.percentile(arr, 50)
+        p90 = np.percentile(arr, 90)
+        assert p50 <= 35000 # 35 ms == 35000 microseconds
+        assert p90 <= 50000 # 50 ms == 50000 microseconds
