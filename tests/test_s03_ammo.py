7dcdf5be04522bb8a3eb63e85e1df0a207e6f765
Oleg Skinderev <>
2022-11-17 13:38:01 +0300

    test

--- tests/test_s03_ammo.py
+++ tests/test_s03_ammo.py
@@ -1,4 +1,5 @@
-import os, glob
+import os
+import glob
 
 from pathlib import Path
 
@@ -11,7 +12,7 @@ def directory():
 
 
 def test_only_200(directory):
-    logdirname = max(glob.glob(os.path.join(os.environ['DIRECTORY'], '*/')), key=os.path.getctime)
+    logdirname = max(glob.glob(os.path.join(directory, '*/')), key=os.path.getctime)
     filename = ''
     for file_name in os.listdir(logdirname):
         name, end = os.path.splitext(file_name)
@@ -20,5 +21,6 @@ def test_only_200(directory):
 
     with open(os.path.join(logdirname, filename)) as phout:
         lines = phout.readlines()
+        print(lines[:10])
         for line in lines:
             assert line.split()[-1] == '200'
