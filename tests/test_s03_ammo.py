12af8ce05f181d15381781a09ba1fe20d5b53c08
Nickolay Kolesnikov <nick206265@gmail.com>
2022-11-17 10:22:44 +0300

    tests

---
+++ tests/test_s03_ammo.py
@@ -0,0 +1,25 @@
+import os, glob
+
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def directory():
+    return Path(os.environ['DIRECTORY'])
+
+
+def test_only_200(directory):
+    logdirname = max(glob.glob(os.path.join(os.environ['DIRECTORY'], '*/')), key=os.path.getctime)
+    filename = ''
+    for file_name in os.listdir(logdirname):
+        name, end = os.path.splitext(file_name)
+        if name.startswith('phout_') and end == '.log':
+            filename = file_name
+
+    with open(os.path.join(logdirname, filename)) as phout:
+        lines = phout.readlines()
+        for line in lines:
+            if line.split()[-1] != '200':
+                print(line.split()[-1])
