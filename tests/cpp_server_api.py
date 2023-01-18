08065ad7cea0e9221dae6ac824b5d3d76859758f
Alexey Konstantinov <konstantinov.py@gmail.com>
2023-01-19 00:10:04 +0300

    Tinkering with container's name

--- tests/cpp_server_api.py
+++ tests/cpp_server_api.py
@@ -150,7 +150,7 @@ class CppServer:
                 if current_time - start_time >= 3:
                     raise Exception({'message': 'Cannot get the right start phrase from the container.',
                                      'logs': logs})
-
+            self.container.rename('new_name')
             if server_domain != '127.0.0.1':
                 server_domain = inspector.inspect_container(self.container.id)['Name'][1:]
             else:
