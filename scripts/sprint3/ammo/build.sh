af77e92ebadf16b98403c10185f6a723c15f48a1
Alexey Konstantinov <konstantinov.py@gmail.com>
2023-02-21 18:49:15 +0300

    Add ammo tests

--- scripts/sprint3/ammo/build.sh
+++ scripts/sprint3/ammo/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+function real_dir() {
+  pushd "$1" >/dev/null
+  pwd -P
+  popd >/dev/null
+}
 
-BASE_DIR=${PWD}
+SCRIPT_FOLDER=$(real_dir "$(dirname "$0")")
+BASE_DIR=${SCRIPT_FOLDER}/../../../../
 SOLUTION_FOLDER=${BASE_DIR}/sprint3/problems/ammo/solution
-SCRIPT_FOLDER=${BASE_DIR}/cpp-backend-tests-practicum/scripts/sprint3/ammo
 
 cp ${BASE_DIR}/cpp-backend-tests-practicum/tests/test_s03_ammo.py ${SCRIPT_FOLDER}/test_s03_ammo.py
 cp -r ${SOLUTION_FOLDER} ${SCRIPT_FOLDER}/solution
--- scripts/sprint3/ammo/run.sh
+++ scripts/sprint3/ammo/run.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-BASE_DIR=${PWD}
+function real_dir() {
+  pushd "$1" >/dev/null
+  pwd -P
+  popd >/dev/null
+}
+
+SCRIPT_FOLDER=$(real_dir "$(dirname "$0")")
+BASE_DIR=${SCRIPT_FOLDER}/../../../../
 SOLUTION_FOLDER=${BASE_DIR}/sprint3/problems/ammo/solution
-SCRIPT_FOLDER=${BASE_DIR}/cpp-backend-tests-practicum/scripts/sprint3/ammo
+
+NETWORK_NAME=ammo_network
+
+docker network rm ${NETWORK_NAME}
+docker network create ${NETWORK_NAME}
+
+echo $(docker run --rm -d --network=${NETWORK_NAME} --name=cpp_server praktikumcpp/game_server:latest)
 
 bash ${SCRIPT_FOLDER}/build.sh
 
 export DIRECTORY=/solution/logs
 
+docker run --rm --privileged --network=${NETWORK_NAME} -e DIRECTORY=${DIRECTORY} ammo
 
-docker run --rm -e DIRECTORY=${DIRECTORY} ammo
+docker container stop cpp_server
+docker network rm ${NETWORK_NAME}
 
 rm -r ${SCRIPT_FOLDER}/solution ${SCRIPT_FOLDER}/test_s03_ammo.py
