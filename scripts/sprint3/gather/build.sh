47e88d8b886a716b97650ae58cd864f811bb1909
Alexey Konstantinov <konstantinov.py@gmail.com>
2023-03-01 02:24:38 +0300

    Fix gather

--- scripts/sprint3/gather/build.sh
+++ scripts/sprint3/gather/build.sh
@@ -12,4 +12,4 @@ SOLUTION_FOLDER=${BASE_DIR}/sprint3/problems/gather/solution
 CPP_FOLDER=${BASE_DIR}/cpp-backend-tests-practicum/tests/cpp/test_s03_gather
 
 cp -r ${CPP_FOLDER}/* ${SOLUTION_FOLDER}/src/
-docker build -t gather . ${SOLUTION_FOLDER}
+docker build -t gather ${SOLUTION_FOLDER}
