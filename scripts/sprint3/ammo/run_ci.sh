032946f97afd01dcbd89c4c15985e8f47e6e14b1
Alexey Konstantinov <konstantinov.py@gmail.com>
2023-03-01 03:20:55 +0300

    Fix run_ci

--- scripts/sprint3/ammo/run_ci.sh
+++ scripts/sprint3/ammo/run_ci.sh
@@ -10,7 +10,7 @@ SCRIPT_FOLDER=$(real_dir "$(dirname "$0")")
 BASE_DIR=${SCRIPT_FOLDER}/../../../../
 SOLUTION_FOLDER=${BASE_DIR}/sprint3/problems/ammo/solution
 
-export DIRECTORY=/solution/logs
-cd sprint3/problems/ammo/solution
+export DIRECTORY=${SOLUTION_FOLDER}/logs
+cd ${SOLUTION_FOLDER}
 yandex-tank -c ${BASE_DIR}/sprint3/problems/ammo/solution/load.yaml ${BASE_DIR}/sprint3/problems/ammo/solution/ammo.txt
 pytest --junitxml=${BASE_DIR}/ammo.xml ${BASE_DIR}/cpp-backend-tests-practicum/tests/test_s03_ammo.py
